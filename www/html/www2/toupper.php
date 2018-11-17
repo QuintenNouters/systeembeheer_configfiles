c8613428900ae7609649d1b1bec3452a9dec8bdb
QuintenNouters <quinten.nouters@student.ucll.be>
2018-11-17 17:12:01 +0100

    changed the location of the vhosts from /var/www to /etc/www. Also changed the references in the files to /etc/www/...

---
+++ www/html/www2/toupper.php
@@ -0,0 +1,6 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.or
+<html xmlns="http://www.w3.org/1999/xhtml">
+<body>
+        <h1>code is: <?php echo strtoupper($_GET['code']); ?></h1>
+</body>
+</html>
